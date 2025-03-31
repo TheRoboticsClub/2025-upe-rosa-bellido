26288389dfbea2d5804dbb3820af6a85d3f084c4
GhYB30 <rgb.celeste@gmail.com>
2025-03-31 13:51:34 +0200

    update

--- docs/_site/assets/js/data/swconf.js
+++ docs/_site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743420420',resources: [
+    cacheName: 'chirpy-1743421879',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
